431f2806e8bae4e9dcb6d6b1963d520a24d24550
Javier <carriloruiz@gmail.com>
2025-11-10 15:34:59 +0100

    Conceptos Basico de PHP

---
+++ Curso_php/Conceptos_Basicos/casting.php
@@ -0,0 +1,31 @@
+<?php
+
+
+//$numerito = "5";
+//
+//var_dump($numerito);
+//
+//$numerito = (int) $numerito;
+//var_dump($numerito);
+
+//$dias = 5.89;
+//var_dump($dias);
+//
+//$dias = (int) $dias;
+//var_dump($dias);
+
+//$banderita = 0 ;
+//$banderita = (bool)$banderita;
+//var_dump($banderita);
+
+$numeros = 5 + "5";
+var_dump($numeros);
+$numeros = "5" + 5;
+var_dump($numeros);
+
+$promedio = $numeros / 1.0;
+var_dump($promedio);
+$michis = 3 + "5 michis";
+var_dump($michis);
+
+echo "\n";
---
+++ Curso_php/Conceptos_Basicos/variables.php
@@ -0,0 +1,16 @@
+<?php
+
+$numero_1 = 8;
+$numero_2 = 7;
+
+echo $numero_1 + $numero_2;
+
+define("NUMERO_PI", 13.14);
+//define("NUMERO_PI", 14);
+
+
+echo NUMERO_PI;
+
+echo "\n";
+
+
